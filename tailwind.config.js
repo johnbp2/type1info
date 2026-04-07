ba12ed633b89fc5bf0b687074ae2c1affe903c15
John Pearson <104274223+johnbp2@users.noreply.github.com>
2026-04-06 20:37:08 -0500

    icons

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector', // Use 'selector' strategy for manual toggling
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
